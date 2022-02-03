d8602fb655661c49c8b6e4c4feca04e85d584926
Chuck Horgan <chorgan182@gmail.com>
2022-02-02 21:13:11 -0500

    Basic app working locally

--- playlist_preserver_app.py
+++ playlist_preserver_app.py
@@ -22,33 +22,57 @@ os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:9090"
 # %% spotify connection
 
 # set scope and establish connection
-scope = "user-read-currently-playing"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
+#scope = "user-read-currently-playing"
+#scope = "user-read-playback-state"
+scope_user = "user-read-private"
+scope_plist_read = "playlist-read-private"
+scope_plist_write = "playlist-modify-private"
+sp_pl_read = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope_plist_read))
+sp_pl_write = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope_plist_write))
+sp_user = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope_user))
 
 # %% app prep
 
 # get user playlists
-user_id = sp.current_user()["id"]
+user_id = sp_user.current_user()["id"]
+
+playlists = sp_pl_read.user_playlists(user_id)
+playlist_names = [x["name"] for x in playlists["items"]]
 
 # bc results are paginated, need to define a func to get all results
-def get_all_playlists(user_id):
-    results = sp.user_playlists(user=user_id)
-    playls = results["items"]
-    while results["next"]:
-        results = sp.next(results)
-        playls.extend(results["items"])
-    return playls
+### should implement eventually but I only have 30
+# def get_all_playlists(user_id):
+#     results = sp.user_playlists(user=user_id)
+#     playls = results["items"]
+#     while results["next"]:
+#         results = sp.next(results)
+#         playls.extend(results["items"])
+#     return playls
     
-playlists = get_all_playlists(user_id)
-#playlist_names = [x["name"] for x in playlists["items"]]
+#playlists = get_all_playlists(user_id)
 
-test = sp.user_playlists(user_id)
-test2 = sp.current_user_playing_track()
 # %% app UI
 
 st.title("Spotify Playlist Preserver")
 
-st.text("Please pick a playlist (note the limit is 50)")
+st.text("Please pick a playlist to modify")
 
-st.selectbox("Playlist: ", playlists)
+st.selectbox("Playlist: ", playlist_names)
+
+
+
+
+
+# radio button
+# first argument is the title of the radio button
+# second argument is the options for the ratio button
+status = st.radio("Is this the coolest thing you've ever seen?",
+                  ('Yes', 'Yes but with more words'))
+ 
+# conditional statement to print
+# Male if male is selected else print female
+# show the result using the success function
+if (status == 'Yes'):
+    st.success("Thank you")
+else:
+    st.success("Thank you much")
