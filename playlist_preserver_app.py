c5044e28d17c73d2dc4940fef533b55eb997a1cc
Chuck Horgan <chorgan182@gmail.com>
2022-02-05 23:57:58 -0500

    Working app with selenium authentication

--- playlist_preserver_app.py
+++ playlist_preserver_app.py
@@ -11,6 +11,11 @@ Created on Tue Feb  1 19:35:20 2022
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import quote
 
 # %% spotify connection
 
@@ -29,52 +34,103 @@ oauth = SpotifyOAuth(scope=scopes,
                      redirect_uri=uri,
                      client_id=cid,
                      client_secret=csecret)
-sp = spotipy.Spotify(oauth_manager=oauth)
 
-# # retrieve auth url
-# auth_url = oauth.get_authorize_url()
+# sign in function
+def get_token(oauth):
+    
+    # retrieve auth url
+    auth_url = oauth.get_authorize_url()
+    
+    # open the auth link in a new window
+    s = Service("./chromedriver.exe")
+    driver = webdriver.Chrome(service=s)
+    driver.get(auth_url)
+    
+    # wait until the user inputs creds and the url changes
+    encoded_uri = quote(uri, safe="")
+    WebDriverWait(driver, 120).until(EC.url_contains(encoded_uri),
+                                    "Sign in timed out")
+    # get the new url
+    response_url = driver.current_url
+    # close window
+    driver.quit()
+    
+    # parse the token from the response url
+    code = oauth.parse_response_code(response_url)
+    token = oauth.get_access_token(code, as_dict=False)
+    
+    # return the token
+    return token
 
-# # sign in function
-# def sign_in(auth_url):
-#     # open the auth link in a modal window
+def sign_in(token):
+    sp = spotipy.Spotify(auth=token)
+    return sp
     
-    
-#     # get a response from the redirect uri
-#     response = "idk get it from the redirect uri"
-    
-#     # parse the token from the link
-#     code = oauth.parse_response_code(response)
-#     token_info = oauth.get_access_token(code)
-#     token = token_info["access_token"]
-    
-#     # return a spotify object    
-#     return spotipy.Spotify(auth=token,)
 
-# test = spotipy.Spotify(oauth_manager=oauth)
-# test.current_user()
+### doesn't work rn, wanted to disable the button with this func on callback
+# def check_sign_in():
+#     try:
+#         id_check = sp.me()["id"]
+#     except:
+#         id_check = None
+        
+#     if id_check is not None:
+#         st.session_state["signed_in"] = True
+#     else:
+#         st.session_state["signed_in"] = False
+
 # %% app UI auth
 
 st.title("Spotify Playlist Preserver")
 
-#sp = st.button("Sign in to Spotify", on_click=sign_in(auth_url))
+# initialize session variables
+if "signed_in" not in st.session_state:
+    st.session_state["signed_in"] = False
+if "cached_token" not in st.session_state:
+    st.session_state["cached_token"] = None
 
-# %% test auth
-
-username = sp.current_user()["id"]
-st.write("this should work")
-st.write("Your username is %s" % username)
-
-st.write("Please pick a playlist to modify")
-
-playlists = sp.user_playlists(username)
-playlist_names = [x["name"] for x in playlists["items"]]
-st.selectbox("Playlist: ", playlist_names)
-
-# %% testing other features
-status = st.radio("Is this the coolest thing you've ever seen?",
-                  ('Yes', 'Yes but with more words'))
-
-if (status == 'Yes'):
-    st.success("Thank you")
+# attempt sign in with cached token
+if st.session_state["cached_token"] is not None:
+    sp = sign_in(st.session_state["cached_token"])
 else:
-    st.success("Thank you much")
+    st.write("No tokens found for this session. Please log in below.")
+
+# display the login button
+sign_in_clicked = st.button("Sign in to Spotify",
+                            disabled=st.session_state["signed_in"])
+
+# log in once the button is clicked
+# save the token in this session to prevent multiple sign-ins
+if sign_in_clicked and "sp" not in locals():
+    try:
+        token = get_token(oauth)
+        sp = sign_in(token)
+        st.session_state["cached_token"] = token
+    except:
+        st.write("An error occurred during authentication!")
+    else:
+        st.session_state["signed_in"] = True
+    
+# only display the following after login
+if "sp" in locals():
+    user = sp.current_user()
+    name = user["display_name"]
+    username = user["id"]
+
+    st.markdown("Hi {n}! Let's modify a playlist or two :smiley:".format(n=name))
+    st.write("Your username is {u}".format(u=username))
+
+    st.write("Please pick a playlist to modify")
+
+    playlists = sp.user_playlists(username)
+    playlist_names = [x["name"] for x in playlists["items"]]
+    st.selectbox("Playlist: ", playlist_names)
+
+    # %% testing other features
+    status = st.radio("Is this the coolest thing you've ever seen?",
+                      ('Yes', 'Yes but with more words'))
+
+    if (status == 'Yes'):
+        st.success("Thank you")
+    else:
+        st.success("Thank you much")
