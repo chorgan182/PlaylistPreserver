15e05c03e5cdbec591806c963930bb349ef351c0
Chuck Horgan <chorgan182@gmail.com>
2022-02-08 20:42:20 -0500

    Code organization and cleanup after successful auth

--- playlist_preserver_app.py
+++ playlist_preserver_app.py
@@ -75,44 +75,56 @@ def app_sign_in():
         st.session_state["signed_in"] = True
     return sp
 
-# %% app auth
+# %% app session variable initialization
 
-st.title("Spotify Playlist Preserver")
-
-st.markdown("""
-Welcome! :wave: This app uses the Spotify API interact with music info and 
-eventually, your playlists! In order to view and modify information associated
-with your account, you must log in. You only need to do this once. Even if no 
-tokens are found, if you are signed in on this browser, you'll just be
-redirected to the app after clicking the link.
-
-_Note: Unfortunately, the current version of Streamlit will not allow for
-staying on the same page, so the authorization and redirection will open in a 
-new tab. This has already been addressed in a development release, so it should
-be implemented in Streamlit Cloud soon!_
-""")
-
-# initialize session variables
 if "signed_in" not in st.session_state:
     st.session_state["signed_in"] = False
 if "cached_token" not in st.session_state:
     st.session_state["cached_token"] = ""
 if "code" not in st.session_state:
     st.session_state["code"] = ""
+    
+# %% app intro
 
-# get current url params
+st.title("Spotify Playlist Preserver")
+
+welcome_msg = """
+Welcome! :wave: This app uses the Spotify API interact with music info and 
+eventually, your playlists! In order to view and modify information associated
+with your account, you must log in. You only need to do this once. Even if no 
+tokens are found, if you are signed in on this browser, you'll just be
+redirected to the app after clicking the link.
+"""
+note_temp = """
+_Note: Unfortunately, the current version of Streamlit will not allow for
+staying on the same page, so the authorization and redirection will open in a 
+new tab. This has already been addressed in a development release, so it should
+be implemented in Streamlit Cloud soon!_
+"""
+
+st.markdown(welcome_msg)
+
+if not st.session_state["signed_in"]:
+    st.markdown(note_temp)
+
+# %% authenticate with response stored in url
+
+# get current url (stored as dict)
 url_params = st.experimental_get_query_params()
 
 # attempt sign in with cached token
 if st.session_state["cached_token"] != "":
     sp = app_sign_in()
-# if no token, but code in url, get code, token, and sign in
+# if no token, but code in url, get code, parse token, and sign in
 elif "code" in url_params:
+    # all params stored as lists, see doc for explanation
     st.session_state["code"] = url_params["code"][0]
     app_get_token()
     sp = app_sign_in()
 # otherwise, prompt for redirect
 else:
+    # this SHOULD open the link in the same tab when Streamlit Cloud is updated
+    # via the "_self" target
     st.write(" ".join(["No tokens found for this session. Please log in by",
                       "clicking the link below."]))
     link_html = " <a target=\"_self\" href=\"{url}\" >{msg}</a> ".format(
@@ -121,28 +133,19 @@ else:
     )
     st.markdown(link_html, unsafe_allow_html=True)
     
-# %% app after auth
+# %% after auth, get user info
 
 # only display the following after login
-if "sp" in locals():
+### is there another way to do this? clunky to have everything in an if:
+if st.session_state["signed_in"]:
     user = sp.current_user()
     name = user["display_name"]
     username = user["id"]
 
     st.markdown("Hi {n}! Let's modify a playlist or two :smiley:".format(n=name))
-    st.write("Your username is {u}".format(u=username))
 
     st.write("Please pick a playlist to modify")
 
     playlists = sp.user_playlists(username)
     playlist_names = [x["name"] for x in playlists["items"]]
     st.selectbox("Playlist: ", playlist_names)
-
-    # %% testing other features
-    status = st.radio("Is this the coolest thing you've ever seen?",
-                      ('Yes', 'Yes but with more words'))
-
-    if (status == 'Yes'):
-        st.success("Thank you")
-    else:
-        st.success("Thank you much")
